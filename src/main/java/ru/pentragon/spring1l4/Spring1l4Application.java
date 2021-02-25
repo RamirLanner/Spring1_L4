6582478b29ede48a1b18630e900123954b06be73
RamirLanner <pentragon@yandex.ru>
2021-02-25 22:12:12 +0300

    hw4

---
+++ src/main/java/ru/pentragon/spring1l4/Spring1l4Application.java
@@ -0,0 +1,13 @@
+package ru.pentragon.spring1l4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring1l4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring1l4Application.class, args);
+    }
+
+}
